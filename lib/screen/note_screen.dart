f29be97e170d6576a2e22cfb368bab6a5ff486b1
Sachin Kumar <ojaz07860@gmail.com>
2022-11-27 00:46:16 +0530

    Added functionality to search note.

--- lib/screen/note_screen.dart
+++ lib/screen/note_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_one/utils/custom_buttom.dart';
 import 'package:note_one/utils/note_tile.dart';
-
 import '../models/note_model.dart';
 
 class NoteScreen extends StatefulWidget {
@@ -12,27 +11,17 @@ class NoteScreen extends StatefulWidget {
 }
 
 class _NoteScreenState extends State<NoteScreen> {
-  final _controller = TextEditingController();
-
   Icon cusIcon = const Icon(Icons.search);
   Widget cusText = const Text('NOTE ONE');
   Icon cusBackButton = const Icon(Icons.arrow_back_ios);
 
   final noteListItem = Note.noteList();
 
-  List searchResult = [];
+  List<Note> searchResult = [];
 
-  void saveNewTask() {
+  void deleteTask(String index) {
     setState(() {
-      noteListItem.add(_controller.text);
-      _controller.clear();
-    });
-    Navigator.of(context).pop();
-  }
-
-  void deleteTask(int index) {
-    setState(() {
-      noteListItem.removeAt(index);
+      noteListItem.removeWhere((element) => element.id == index);
     });
   }
 
@@ -47,8 +36,8 @@ class _NoteScreenState extends State<NoteScreen> {
   }
 
   void _runFilter(String resultKeyword) {
-    List result = [];
-    if (result.isEmpty) {
+    List<Note> result = [];
+    if (resultKeyword.isEmpty) {
       result = noteListItem;
     } else {
       result = noteListItem
@@ -68,62 +57,69 @@ class _NoteScreenState extends State<NoteScreen> {
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 27, 27, 36),
+        appBar: AppBar(
+          leadingWidth: 80,
+          titleSpacing: 0,
+          leading: cusBackButton,
+          elevation: 0,
           backgroundColor: const Color.fromARGB(255, 27, 27, 36),
-          appBar: AppBar(
-            leadingWidth: 80,
-            titleSpacing: 0,
-            leading: cusBackButton,
-            elevation: 0,
-            backgroundColor: const Color.fromARGB(255, 27, 27, 36),
-            title: cusText,
-            centerTitle: true,
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 28),
-                child: IconButton(
-                  icon: cusIcon,
-                  onPressed: () {
-                    setState(
-                      () {
-                        if (cusIcon.icon == Icons.search) {
-                          cusBackButton = const Icon(Icons.search);
-                          cusIcon = const Icon(Icons.clear);
-                          cusText = TextField(
-                            onChanged: (value) => _runFilter(value),
-                            decoration: const InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Search your Note",
-                              hintStyle: TextStyle(color: Colors.white),
-                            ),
-                            style: const TextStyle(color: Colors.white),
-                          );
-                        } else {
-                          cusIcon = const Icon(Icons.search);
-                          cusText = const Text('NOTE ONE');
-                          cusBackButton = const Icon(Icons.arrow_back_ios);
-                        }
-                      },
-                    );
-                  },
+          title: cusText,
+          centerTitle: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 28),
+              child: IconButton(
+                icon: cusIcon,
+                onPressed: () {
+                  setState(
+                    () {
+                      if (cusIcon.icon == Icons.search) {
+                        cusBackButton = const Icon(Icons.search);
+                        cusIcon = const Icon(Icons.clear);
+                        cusText = TextField(
+                          onChanged: (value) => _runFilter(value),
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search your Note",
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(color: Colors.white),
+                        );
+                      } else {
+                        _runFilter('');
+                        cusIcon = const Icon(Icons.search);
+                        cusText = const Text('NOTE ONE');
+                        cusBackButton = const Icon(Icons.arrow_back_ios);
+                      }
+                    },
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+        body: searchResult.isNotEmpty
+            ? ListView.builder(
+                itemCount: searchResult.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ToDoTile(
+                    taskName: searchResult[index].noteText.toString(),
+                    taskDescription:
+                        searchResult[index].noteDescription.toString(),
+                    deleteTask: (context) => deleteTask,
+                    editTask: (context) => editTask(index),
+                  );
+                })
+            : const Center(
+                child: Text(
+                  "No Note Found!",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
-              )
-            ],
-          ),
-          body: searchResult.isNotEmpty
-              ? ListView.builder(
-                  itemCount: noteListItem.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ToDoTile(
-                      taskName: noteListItem[index].toString(),
-                      deleteTask: (context) => deleteTask(index),
-                      editTask: (context) => editTask(index),
-                    );
-                  })
-              : const Text("No Note Found!"),
-          // : Text('No Result Found!'),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: const CustomButton()),
+              ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: const CustomButton(),
+      ),
     );
   }
 }
